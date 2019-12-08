dd566a884d62395b989925f8e152f9f44d0ec454
Rain Catalpa <56062949+CatalpaRain@users.noreply.github.com>
2019-12-08 06:05:48 -0600

    后缀表达式求值

---
+++ 数据结构第二次上机后缀表达式求值（中缀转后缀)/后缀表达式求值.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<conio.h>
+#define add 43
+#define subs 45
+#define mult 42
+#define div 47
+#define MAXSIZE 100
+typedef sturuct{
+	int data{MAXSIZE};//数组表示栈空间，长度为maxsize的堆栈
+int top;
+}seqstack;
+seqstack* s;
+seqstack* Init()//初始化栈
+{
+	seqstack 
+}
